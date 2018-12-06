2fa7fdbd4929b52f224f8c221e54e586a786a976
Jeremy Yiu <utsjeremy@gmail.com>
2018-12-07 01:37:01 +1100

    added basic functionality

---
+++ app/src/main/java/com/example/jyiu/justjava/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.jyiu.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    int numberOfCoffees = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        String priceMessage = "Total: $" + (numberOfCoffees * 5) + "\nThank you.";
+        displayMessage(priceMessage);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+
+    public void increment(View view) {
+        numberOfCoffees++;
+        display(numberOfCoffees);
+    }
+
+    public void decrement(View view) {
+        numberOfCoffees--;
+        display(numberOfCoffees);
+    }
+}
